6a4407cad0c67277ff31b16856ad7a8d5696d6bf
NeyasG <neyasg@gmail.com>
2023-02-10 21:42:54 +0000

    Fixed Desktop Nav bar and added toggle function

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 const body = document.querySelectorAll("body, div.img-caption, a.icon");
+const closedIcon = document.getElementById("mobile-close-icon");
+const hamburgerIcon = document.getElementById("ham-icon");
+
+function toggleStyle(ele, styleName, value) {
+    if (ele.style[styleName] !== value) {
+        ele.style[styleName] = value;
+    } else {
+        ele.style[styleName] = '';
+    }
+
+}
 
 navToggle.addEventListener('click', () => {
     primaryNav.classList.toggle('opened');
-    // body.classList.toggle('blurred');
-    console.log(body);
+
+
+
+    toggleStyle(closedIcon, "display", "inline");
+    toggleStyle(hamburgerIcon, "display", "none")
 
     body.forEach((ele) => {
         ele.classList.toggle('blurred');
